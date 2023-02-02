8645fa706f894404aebb9609868e5c2464d506de
SKALE-jace <skalelabs-jace@Jaces-MacBook-Pro.local>
2023-02-02 16:55:18 -0600

    first commit

---
+++ presets.js
@@ -0,0 +1,18 @@
+window.SKLUI_PRESETS = {
+    optimize: {
+        prefetchEvent: 'dirLoad',
+        prefetchDepth: Infinity,
+    },
+    branding: {
+        logoUrl: 'logo.png',
+        logoText: 'SKALE',
+        greetingText: 'Filestorage'
+    },
+    navigator: {
+        pageLimit: 10
+    },
+    uploader: {
+        batchThreshold: 20,
+        maxFileDirNameLength: 255
+    }
+}
